3f0c4939abdbe22610d5a8967e973a271451caa8
7lad48 <2335213@gmail.com>
2024-04-20 14:16:11 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,43 @@
 import { defineConfig } from 'vite';
+//const baseLink = import.meta.env.MODE === 'development' ? '/' : '/arta/';
+//  export default defineConfig({
+//      base: baseLink
+//  })
 
-const config = {
-    server: {
-        base: '/',
-    },
-    build: {
-        base: '/arta/',
-    },
-};
-
-export default ({ mode }) => {
-    if (mode === 'development') {
-        return defineConfig(config);
+export default ({mode}) => {
+    if(mode === 'development'){
+        return defineConfig({
+            base: '/'
+        })
     } else {
         return defineConfig({
-            ...config,
-            ...{
-                server: {
-                    ...config.server,
-                    base: '/arta/',
-                },
-            },
-        });
+            base: '/arta/'
+        })
     }
-};
+}
+
+//
+// const config = {
+//     server: {
+//         base: '/',
+//     },
+//     build: {
+//         base: '/arta/',
+//     },
+// };
+//
+// export default ({ mode }) => {
+//     if (mode === 'development') {
+//         return defineConfig(config);
+//     } else {
+//         return defineConfig({
+//             ...config,
+//             ...{
+//                 server: {
+//                     ...config.server,
+//                     base: '/arta/',
+//                 },
+//             },
+//         });
+//     }
+// };
